7daa348e41191d4579d8c5d4bae338eb7ad3ebbb
lachiem1 <lachiem1@gmail.com>
2025-07-07 20:48:43 +1000

    refactor subscribe msg into struct and move out uri into env variable

--- cmd/websocketcoinbase/main.go
+++ cmd/websocketcoinbase/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"time"
 
-	"github.com/lachiem1/crowd-sourced-crypto-trading/internal/websocket"
 	gorillaWebSocket "github.com/gorilla/websocket"
+	"github.com/lachiem1/crowd-sourced-crypto-trading/internal/config"
+	"github.com/lachiem1/crowd-sourced-crypto-trading/internal/websocket"
 )
 
+// defining individual channels to subscribe to
+type SubscribeMessage struct {
+	Type 	   string   `json:"type"`
+	ProductIds []string `json:"product_ids"`
+	Channel    string 	`json:"channel"`
+	Signature  string   `json:"signature,omitempty"`
+	Key 	   string   `json:"key,omitempty"`
+	Passphrase string   `json:"passphrase,omitempty"`
+	Timestamp  string   `json:"timestamp,omitempty"`
+}
+
 func main() {
+	env, err := config.LoadEnv()
+	if err != nil {
+		log.Fatal("could not load environment vars, exiting program.")
+	}
+
 	// program shutdown
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
@@ -20,8 +38,8 @@ func main() {
 	client := websocket.NewClient()
 
 	// establish websocket connection
-	uri := "wss://ws-feed.exchange.coinbase.com"
-	err := client.Connect(uri)
+	uri := env.CoinbaseMarketDataUri
+	err = client.Connect(uri)
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
 	}
@@ -32,26 +50,28 @@ func main() {
 
 	// message I send to coinbase via websocket to control which info they send
 	// from coinbase docs: need to send subscribe message < 5 secs after connection established
-	// https://docs.cdp.coinbase.com/exchange/websocket-feed/authentication
-	subscribeMsg := []byte(`{
-			"type": "subscribe",
-			"product_ids": ["ETH-USD", "BTC-USD"],
-			"channels": ["full"],
-			"signature": TODO - sig here,
-			"key": TODO - key env variable here,
-			"passphrase": TODO - passphrase env var here,
-			"timestamp": TODO
-	}`)
+	// https://docs.cdp.coinbase.com/coinbase-app/advanced-trade-apis/websocket/websocket-overview
+	subscribeMsg := SubscribeMessage{
+		Type: "subscribe",
+		ProductIds: []string{"BTC-USD"},
+		Channel: "level2",
+	}
+	log.Println(subscribeMsg)
+
+	subscribeMsgBytes, err := json.Marshal(subscribeMsg)
+	if err != nil {
+		log.Fatalf("failed to convert subscribe message struct to json.")
+	}
+
 	time.Sleep(1 * time.Second)
 	if client.IsConnected() {
-		err = client.WriteMessage(gorillaWebSocket.TextMessage, subscribeMsg)
+		err = client.WriteMessage(gorillaWebSocket.TextMessage, subscribeMsgBytes)
 		if err != nil {
 			log.Printf("error sending subscribe message: %v", err)
 		}
 	} else {
 		log.Println("cannot send subscribe message: websocket not connected")
 	}
-
 
 	<-interrupt
 	log.Println("interrupt signal received, gracefully shutting down")
@@ -60,24 +80,23 @@ func main() {
 // process received messages and errors from websocket connection
 func processMessagesErrors(client *websocket.Client, uri string) {
 	for {
-			select {
-			case msg := <- client.Messages:
-				fmt.Printf("msg received from coinbase websocket: %s\n", msg)
-				// parse JSON to Kafka here - TODO
-			case err := <- client.Errors:
-				fmt.Printf("error received from coinbase websocket: %s\n", err)
+		select {
+		case msg := <- client.Messages:
+			fmt.Printf("msg received from coinbase websocket: %s\n", msg)
+			// parse JSON to Kafka here - TODO
+		case err := <- client.Errors:
+			fmt.Printf("error received from coinbase websocket: %s\n", err)
 
-				if !client.IsConnected() {
-					log.Println("websocket connection lost")
-					// crude singular retry - TODO: retries with exponential backoff
-					time.Sleep(5 * time.Second)
-					err := client.Connect(uri)
-					// if reconnection attempt fails, log it
-					if err != nil {
-						log.Printf("reconnection attempt failed: %v", err)
-					}
+			if !client.IsConnected() {
+				log.Println("websocket connection lost")
+				// crude singular retry - TODO: retries with exponential backoff
+				time.Sleep(5 * time.Second)
+				err := client.Connect(uri)
+				// if reconnection attempt fails, log it
+				if err != nil {
+					log.Printf("reconnection attempt failed: %v", err)
 				}
 			}
-		
 		}
+	}
 }
