d9b74604a8f43f8d4a4c2936936477830ebec3e1
lachiem1 <lachiem1@gmail.com>
2025-11-16 22:00:31 +1100

    create interface for service class

--- user-service/src/main/kotlin/com/lachiem1/userservice/service/UserService.kt
+++ user-service/src/main/kotlin/com/lachiem1/userservice/service/UserService.kt
@@ -1,12 +1,15 @@
 package com.lachiem1.userservice.service
 
-import com.lachiem1.userservice.model.UserClient
-import com.lachiem1.userservice.model.UserServer
-import com.lachiem1.userservice.repository.UserRepository
+import com.lachiem1.userservice.domain.user.UserClient
+import com.lachiem1.userservice.domain.user.UserServer
+import com.lachiem1.userservice.domain.user.UserServiceResult
 
-class UserService(
-    private val repository: UserRepository = UserRepository()
-) {
-    fun getUserSample(): UserServer = repository.getSample()
-    fun createUser(request: UserClient): UserServer = repository.create(request)
+interface UserService {
+    suspend fun validateUserRequestInformation(user: UserClient) : UserServiceResult
+    suspend fun validateUserExists(id: String) : UserServiceResult
+    suspend fun createUser(user: UserClient) : UserServiceResult
+    suspend fun readUserById(id: String) : UserServiceResult
+    suspend fun readAllUsers() : UserServiceResult
+    suspend fun updateUser(user: UserServer) : UserServiceResult
+    suspend fun deleteUser(user: UserServer) : UserServiceResult
 }
