10fbaa8623c1d9886fa3faaf84bfbce128a29315
lachiem1 <lachiem1@gmail.com>
2025-11-16 21:55:55 +1100

    update dependencies to include kotest

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,11 +1,12 @@
+val kotestVersion = "6.0.0"
+val logback_version: String by project
+
 plugins {
     kotlin("jvm") version "2.2.20"
     id("io.ktor.plugin") version "3.3.1"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
     application
 }
-
-val logback_version: String by project
 
 kotlin {
     jvmToolchain(21)
@@ -27,9 +28,20 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:3.3.1")
+    implementation("org.postgresql:postgresql:42.7.8")
+    implementation("com.h2database:h2:2.3.232")
+    implementation("io.ktor:ktor-server-content-negotiation:3.3.1")
+    implementation("io.ktor:ktor-server-core:3.3.1")
 
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("io.ktor:ktor-client-content-negotiation")
     testImplementation("io.ktor:ktor-serialization-kotlinx-json")
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
